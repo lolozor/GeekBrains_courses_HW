d71846266518fa7dcbe9d62f469d6b0d444494a7
lolozor <lolo-zor@yandex.ru>
2020-04-23 10:08:59 +0300

    Алгоритмы_python_lesson1_hw

---
+++ Алгоритмы и структуры данных на PYTHON/lesson_1/lesson1_hw5.py
@@ -0,0 +1,58 @@
+# 5. 
+# Пользователь вводит номер буквы в алфавите. 
+# Определить, какая это буква.
+
+ind = int(input('Введите номер буквы: '))
+
+if ind == 1:
+    print('a')
+if ind == 2:
+    print('b')
+if ind == 3:
+    print('c')
+if ind == 4:
+    print('d')
+if ind == 5:
+    print('e')
+if ind == 6:
+    print('f')
+if ind == 7:
+    print('g')
+if ind == 8:
+    print('h')
+if ind == 9:
+    print('i')
+if ind == 10:
+    print('j')
+if ind == 11:
+    print('k')
+if ind == 12:
+    print('l')
+if ind == 13:
+    print('m')
+if ind == 14:
+    print('n')
+if ind == 15:
+    print('o')
+if ind == 16:
+    print('p')
+if ind == 17:
+    print('q')
+if ind == 18:
+    print('r')
+if ind == 19:
+    print('s')
+if ind == 20:
+    print('t')
+if ind == 21:
+    print('u')
+if ind == 22:
+    print('v')
+if ind == 23:
+    print('w')
+if ind == 24:
+    print('x')
+if ind == 25:
+    print('y')
+if ind == 26:
+    print('z')
